eaec9395c8e3fb8c0cccd3a3bdf69017ddb1e9f6
Rufus <rufusgor@gmail.com>
2014-12-05 01:52:58 +0300

    modify proxy.sh
    убрал, оказалось не практично.
    Лучше запускать через cron.

--- proxy.sh
+++ proxy.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
-while true
-	do
-		PROXY_HOST=localhost
-		PROXY_PORT=61111
 
-		SSHD_HOST=host.ru
-		SSHD_USER=root
-		SSHD_PORT=22
+PROXY_HOST=localhost
+PROXY_PORT=61111
 
-		COMMAND="ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
+SSHD_HOST=host.ru
+SSHD_USER=root
+SSHD_PORT=22
+
+COMMAND="ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
 
 if nc -z $PROXY_HOST $PROXY_PORT
-	then
-		echo "Proxy on $PROXY_HOST:$PROXY_PORT up!"
-		#echo "ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
-	else
- 		echo "Proxy on $PROXY_HOST:$PROXY_PORT is down"
-		killall ssh 2&>1
-		$COMMAND
+then
+    echo "Proxy on $PROXY_HOST:$PROXY_PORT up!"
+	#echo "ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
+else
+    echo "Proxy on $PROXY_HOST:$PROXY_PORT is down"
+	killall ssh 2&>1
+	$COMMAND
 fi
-	sleep 120
-done
+
