65e573c6ca89cdf09d3849804e299ae07cfe720b
Rufus <rufusgor@gmail.com>
2014-12-05 01:41:28 +0300

    modify proxy.sh
    Добавил restart скрипта без crontab

--- proxy.sh
+++ proxy.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
+while true
+	do
+		PROXY_HOST=localhost
+		PROXY_PORT=61111
 
-PROXY_HOST=localhost
-PROXY_PORT=61111
+		SSHD_HOST=host.ru
+		SSHD_USER=root
+		SSHD_PORT=22
 
-SSHD_HOST=host.ru
-SSHD_USER=root
-SSHD_PORT=22
-
-COMMAND="ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
+		COMMAND="ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
 
 if nc -z $PROXY_HOST $PROXY_PORT
-then
-    echo "Proxy on $PROXY_HOST:$PROXY_PORT up!"
-	#echo "ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
-else
-    echo "Proxy on $PROXY_HOST:$PROXY_PORT is down"
-	killall ssh 2&>1
-	$COMMAND
+	then
+		echo "Proxy on $PROXY_HOST:$PROXY_PORT up!"
+		#echo "ssh -q -f -N -D $PROXY_PORT $SSHD_USER@$SSHD_HOST -p$SSHD_PORT"
+	else
+ 		echo "Proxy on $PROXY_HOST:$PROXY_PORT is down"
+		killall ssh 2&>1
+		$COMMAND
 fi
-
+	sleep 120
+done
